f18d800dc6c63967e31f30a22bb6770ca334f446
romangdemyanchuk <roman.g.demyanchuk@gmail.com>
2021-05-07 15:37:42 +0300

    first commit

--- src/redux/session/Auth/AuthActions.js
+++
@@ -1,4 +0,0 @@
-import {LOGIN_CREATOR, REGISTER_CREATOR} from "./AuthConstants";
-
-export const loginCreator = (data) => ({ type: LOGIN_CREATOR});
-export const registerCreator = () => ({ type: REGISTER_CREATOR});
--- src/redux/session/Layout/LayoutActions.js
+++
@@ -1,8 +0,0 @@
-import {ITEMS, RESOURCE, LOADING, GET_ITEMS_CREATOR} from "./LayoutConstants";
-
-
-export const allItems = (data) => ({type: ITEMS, payload: data});
-export const expectedResource = (data) => ({type: RESOURCE, payload: data});
-export const loading = (data) => ({type: LOADING, payload: data});
-export const all = () => ({type: GET_ITEMS_CREATOR});
-
